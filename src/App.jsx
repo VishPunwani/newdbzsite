a820ede8cbe527bebb4c7b55acb7f6b2b617f7b1
VishPunwani <111029896+VishPunwani@users.noreply.github.com>
2024-03-12 19:29:56 -0700

    new react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Loader from './components/Loader';
+
+function App() {
+
+    return (
+      
+        <div className="App"> 
+         <Loader />
+        </div>
+        
+    );
+  }
+
+export default App
---
+++ src/components/Loader.jsx
@@ -0,0 +1,7 @@
+
+
+function Loader(){
+    return <h1>Hello from a Comp!</h1>
+}
+
+export default Loader;
