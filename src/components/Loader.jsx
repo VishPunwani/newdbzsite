df19dcb8f14b2d1b9350f7439f0034f03e78b9f7
VishPunwani <111029896+VishPunwani@users.noreply.github.com>
2024-03-19 19:16:24 -0700

    newchanges

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,5 +1,29 @@
 function Loader() {
-  return <h1>Hello from a Comp!</h1>;
+  return (
+    <>
+      <div class="loader-gray-color">
+        <div class="loader-gray"></div>
+        <div class="loader-color">
+          <img
+            src="https://luisangelmaciel.github.io/dragon.ball/loader/img/Dragon_Ball_Super.png"
+            alt=""
+            width="100%"
+            height="auto"
+            class="loader-color-img"
+            title="lorem"
+          />
+        </div>
+      </div>
+      <center>
+        <quote class="display-topmiddle openSans">
+          <br />
+          <br />. . .<cite></cite>
+          <br />
+          <b></b>
+        </quote>
+      </center>
+    </>
+  );
 }
 
 export default Loader;
